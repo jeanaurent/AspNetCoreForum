3e65db1759e06efc94c643cbed476dbfc7ba0ef8
Jean-BKK <jlcassier@gmail.com>
2020-03-04 14:56:14 +0700

    Adding MessageController and Message Class

---
+++ AspNetCoreForum/Models/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreForum.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public DateTime date { get; set; }
+        public string category  { get; set; }
+        public string author  { get; set; }
+        public string subject  { get; set; }
+        public string content  { get; set; }
+        public string status  { get; set; }
+    }
+}
