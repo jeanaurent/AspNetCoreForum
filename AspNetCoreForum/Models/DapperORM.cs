c4ea847f3448c4d66e38c836d889cd56e6ede844
Jean-BKK <jlcassier@gmail.com>
2020-03-04 13:06:28 +0700

    Create DapperORM class and set the Npgsql Connection

---
+++ AspNetCoreForum/Models/DapperORM.cs
@@ -0,0 +1,19 @@
+﻿using Dapper;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreForum.Models
+{
+    public static class DapperORM
+    {
+        private static string ConnectionString = @"Data Source=localhost;port=5432;Pooling=true;Initial Catalog=ShopDB;User Id= postgres;Password=SunWave2019!;SslMode=none;";
+    
+        public static void ExecuteWithoutReturn(string procedureName, DynamicParameters param)
+        {
+            using(NpgsqlConnection )
+        }
+    }
+}
